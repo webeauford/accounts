7ffadcfe70393b4a5cc5d298c1f7c59b246f0c7f
William Beauford <wbebeauford@gmai.com>
2026-02-14 17:16:54 -0600

    added in new datastructs

--- pkg/accounts/datastructers.go
+++ pkg/accounts/datastructers.go
@@ -1,7 +1,22 @@
 package accounts
 
+import "time"
+
 type User struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Disabled  bool      `json:"disabled"`
+	Roles     []Role    `json:"roles"`
+	CreatedAT time.Time `json:"created_at"`
+}
+
+type Role struct {
+	ID          string      `json:"id"`
+	Name        string      `json:"name"`
+	Permissions []Permisson `json:"permissions"`
+}
+
+type Permisson struct {
+	Code string `json:"code"`
 }
